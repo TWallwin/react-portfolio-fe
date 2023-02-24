d6ae4299ce76752c86228eaf884fde8b4f30dfd2
Tom wallwin <TWallwin>
2023-02-24 12:55:24 +0000

    added scroll snapping

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import Header from "./components/Header"
 import DesktopRoutes from "./components/routers/DesktopRoutes";
 import { useDesktopMediaQuery } from "./helpers/mediaQueryHelpers";
 import MobileRoutes from "./components/routers/MobileRoutes";
-// import useScrollSnap from 'react-use-scroll-snap';
-// import { useRef } from 'react'
+import useScrollSnap from 'react-use-scroll-snap';
+import { useRef } from 'react'
 
 function App() {
   const isDesktop = useDesktopMediaQuery()
 
-  // const scrollRef = useRef(null);
-  // useScrollSnap({ ref: scrollRef, duration: 100, delay: 50 });
+  const scrollRef = useRef(null);
+  useScrollSnap({ ref: scrollRef, duration: 100, delay: 50 });
 
   return (
     <BrowserRouter>
-      <div className="App" >
+      <div className="App" ref={scrollRef} >
         <Header />
         <main>
         <div className='pages'>
